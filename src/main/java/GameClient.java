2c797b785f8877109913e78d0ce14fe1a1b9f4df
Soloement Chu <ej942l4conpe@gmail.com>
2021-01-03 23:28:10 +0800

    Enhance painting code to use gameObject

--- src/main/java/GameClient.java
+++ src/main/java/GameClient.java
@@ -1,3 +1,4 @@
+import object.GameObject;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.KeyEvent;
@@ -8,9 +9,8 @@ public class GameClient extends JComponent {
     private int frameHeight;
     private boolean stop;
     private Image backGroundMap;
+    private ArrayList<GameObject> gameObjects;
     Tank playerTank;
-    ArrayList<Tank> enemyTanks = new ArrayList<>();
-    ArrayList<Wall> walls = new ArrayList<>();
 
     GameClient() {
         this(800, 600);
@@ -34,7 +34,7 @@ public class GameClient extends JComponent {
     }
 
     void init() {
-
+        gameObjects = new ArrayList<>();
         Image[] itankImg = new Image[8];
         Image[] etankImg = new Image[8];
         String[] sub = {"U", "D", "L", "R", "LU", "RU", "LD", "RD"};
@@ -42,33 +42,26 @@ public class GameClient extends JComponent {
             itankImg[i] = Tools.getImage("itank" + sub[i] + ".png");
             etankImg[i] = Tools.getImage("etank" + sub[i] + ".png");
         }
-
         playerTank = new Tank(400, 100, Direction.DOWN,itankImg);
+        gameObjects.add(playerTank);
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 4; j++) {
-                enemyTanks.add(new Tank(250 + 90 * j, 400 + 80 * i, Direction.UP, true,etankImg));
+                gameObjects.add(new Tank(250 + 90 * j, 400 + 80 * i, Direction.UP, true,etankImg));
             }
         }
         Image[] image = {Tools.getImage("brick.png")};
-        walls.add(new Wall(100, 200, true, 15, image));
-        walls.add(new Wall(650, 200, true, 15, image));
-        walls.add(new Wall(200, 200, false, 13, image));
+        gameObjects.add(new Wall(100, 200, true, 15, image));
+        gameObjects.add(new Wall(650, 200, true, 15, image));
+        gameObjects.add(new Wall(200, 200, false, 13, image));
         backGroundMap = Tools.getImage("map_sand.jpg");
     }
 
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-//        g.setColor(Color.BLACK);
-//        g.drawImage(backGroundMap,1024,864,null);
-//        g.fillRect(0, 0, frameWidth, frameHeight);
         g.drawImage(backGroundMap,0,0,null);
-        playerTank.draw(g);
-        for (Tank t : enemyTanks) {
-            t.draw(g);
-        }
-        for (Wall w : walls) {
-            w.draw(g);
+        for (GameObject o:gameObjects) {
+            o.draw(g);
         }
     }
 
